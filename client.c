74d4754f906282642e342913c154b4976a18b4b5
MuLin <Millenary.soul@gmail.com>
2015-03-01 01:54:17 +0800

    Add client.c and server.c to test git function

---
+++ client.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define SERVER_PORT	5432
+#define MAX_LINE	256
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	struct hostent *hp;
+	struct sockaddr_in sin;
+	char *host;
+	char buf[MAX_LINE];
+	char buf_get[MAX_LINE];
+	int s;
+	int len;
+
+	if (argc==2)
+	{
+		host = argv[1];
+	}
+	else
+	{
+		fprintf(stderr, "usage: simplex-talk host\n");
+		exit(1);
+	}
+
+	/* translate host name into peer's IP address */
+	hp = gethostbyname(host);
+	if (!hp)
+	{
+		fprintf(stderr, "simplex-talk: unknown host: %s\n", host);
+		exit(1);
+	}
+
+	/* build address data structure */
+	bzero((char *)&sin, sizeof(sin));
+	sin.sin_family = AF_INET;
+	bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);	//h_addr複製到sin.sin_addr
+	sin.sin_port = htons(SERVER_PORT);
+
+	/* active open */
+	if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("simplex-talk: socket");
+		exit(1);
+	}
+	if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+	{
+		perror("simplex-talk: connect");
+		close(s);
+		exit(1);
+	}
+
+    printf("Please Enter your name:");
+    fgets(buf, sizeof(buf), stdin);
+
+    len = strlen(buf) +1 ;
+    send(s, buf, len, 0);
+
+
+    while(len = recv(s, buf_get, sizeof(buf_get), 0))
+    {
+			printf("\n%s",buf_get);
+    }
+
+}
---
+++ server.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define SERVER_PORT	5432
+#define MAX_PENDING	5
+#define MAX_LINE	256
+
+int main()
+{
+	struct sockaddr_in sin;
+	char buf[MAX_LINE];
+	char message[MAX_LINE];
+	int len;
+	int s, new_s;
+
+	/* build address data structure */
+	bzero((char *)&sin, sizeof(sin));	//清零
+	sin.sin_family = AF_INET;	//AF_INET:使用TCP/IPv4
+	sin.sin_addr.s_addr = INADDR_ANY;	//任何位址
+	sin.sin_port = htons(SERVER_PORT);	//host to network shot int:轉換成網路格式
+
+	/* setup passive open */
+	if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)	//PF_INET:IPv4
+	{
+		perror("simplex-talk: socket");
+		exit(1);
+	}
+	if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0)
+	{
+		perror("simplex-talk: bind");
+		exit(1);
+	}
+	listen(s, MAX_PENDING);
+
+	/* wait for connection, then receive and print text */
+	while(1)
+	{
+		if ((new_s = accept(s, (struct sockaddr *)&sin, &len)) < 0)
+		{
+			perror("simplex-talk: accept");
+			exit(1);
+		}
+
+		len = recv(new_s, buf, sizeof(buf), 0);
+		fputs(buf, stdout);
+
+		strcpy(message,"Welcome! ");
+		strncat(message,buf,len);
+		send(new_s, message, strlen(message)+1, 0);
+
+		printf("DONE!\n");
+
+		close(new_s);
+	}
+}
+
